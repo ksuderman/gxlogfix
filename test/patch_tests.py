fb86f2e4a6a7d90e1502de1d215baf3328571eed
Keith Suderman <suderman@jhu.edu>
2023-10-23 15:54:16 -0400

    Update expected strings to match quoting style used by the ast module.

--- test/patch_tests.py
+++ test/patch_tests.py
@@ -12,7 +12,7 @@ class PatchTestBase(unittest.TestCase):
         assert 1 == len(patches)
         assert 1 in patches
         actual = patches[1].render()
-        assert actual == expected, f"{actual} != {expected}"
+        assert actual == expected, f'{actual} != {expected}'
 
     def assert_unchanged(self, source):
         patches = get_patch(source, "__fake__.py")
@@ -48,33 +48,33 @@ class MethodTests(PatchTestBase):
 
 class ModOpTests(PatchTestBase):
     def test_modop(self):
-        source = 'log.debug("%s" % a)'
-        expected = 'log.debug("%s", a)'
+        source = "log.debug('%s' % a)"
+        expected = "log.debug('%s', a)"
         self.assert_patched(source, expected)
 
     def test_modop_3(self):
-        source = 'log.debug("%s %s %s" % (a, b, c))'
-        expected = 'log.debug("%s %s %s", a, b, c)'
+        source = "log.debug('%s %s %s' % (a, b, c))"
+        expected = "log.debug('%s %s %s', a, b, c)"
         self.assert_patched(source, expected)
 
     def test_modop_3_functions(self):
-        source = 'log.debug("%d %f %s" % (a+b, log(a), foo(bar(x,y))))'
-        expected = 'log.debug("%d %f %s", a + b, log(a), foo(bar(x, y)))'
+        source = "log.debug('%d %f %s' % (a+b, log(a), foo(bar(x,y))))"
+        expected = "log.debug('%d %f %s', a + b, log(a), foo(bar(x, y)))"
         self.assert_patched(source, expected)
 
     def test_modop_from_tasks_py(self):
-        source = 'log.debug("_cancel_job for job %d: Stopping running task %d" % (job.id, task.id))'
-        expected = 'log.debug("_cancel_job for job %d: Stopping running task %d", job.id, task.id)'
+        source = "log.debug('_cancel_job for job %d: Stopping running task %d' % (job.id, task.id))"
+        expected = "log.debug('_cancel_job for job %d: Stopping running task %d', job.id, task.id)"
         self.assert_patched(source, expected)
 
     def test_modof_value_formats(self):
-        source = 'log.debug("%2.3f %03d %-20s" % (3.14, 1, "hello"))'
-        expected = "log.debug(\"%2.3f %03d %-20s\", 3.14, 1, 'hello')"
+        source = "log.debug('%2.3f %03d %-20s' % (3.14, 1, 'hello'))"
+        expected = "log.debug('%2.3f %03d %-20s', 3.14, 1, 'hello')"
         self.assert_patched(source, expected)
 
     def test_preserve_format_string_modop_3(self):
-        source = 'log.info("%s %d %f" % (a, b, c))'
-        expected = 'log.info("%s %d %f", a, b, c)'
+        source = "log.info('%s %d %f' % (a, b, c))"
+        expected = "log.info('%s %d %f', a, b, c)"
         self.assert_patched(source, expected)
 
     def test_mod_op_2_named_parameters(self):
@@ -85,62 +85,64 @@ class ModOpTests(PatchTestBase):
 
 class PatchFStringTest(PatchTestBase):
     def test_fstring(self):
-        source = 'log.debug(f"hello {world}")'
-        expected = 'log.debug("hello %s", world)'
+        source = "log.debug(f'hello {world}')"
+        expected = "log.debug('hello %s', world)"
         self.assert_patched(source, expected)
 
     def test_fstring_with_no_substitutions(self):
-        source = 'log.debug(f"hello world")'
-        expected = 'log.debug("hello world")'
+        source = "log.debug(f'hello world')"
+        expected = "log.debug('hello world')"
         self.assert_patched(source, expected)
 
     def test_fstring_formatted_value_f(self):
-        source = 'log.debug(f"pi is {pi:2.3f}")'
-        expected = 'log.debug("pi is %2.3f", pi)'
+        source = "log.debug(f'pi is {pi:2.3f}')"
+        expected = "log.debug('pi is %2.3f', pi)"
         self.assert_patched(source, expected)
 
     def test_fstring_formatted_value_d(self):
-        source = 'log.debug(f"x is {x:03d}")'
-        expected = 'log.debug("x is %03d", x)'
+        source = "log.debug(f'x is {x:03d}')"
+        expected = "log.debug('x is %03d', x)"
         self.assert_patched(source, expected)
 
     def test_fstring_formatted_value_s(self):
-        source = 'log.debug(f"hello {world:20}")'
-        expected = 'log.debug("hello %20s", world)'
+        source = "log.debug(f'hello {world:20}')"
+        expected = "log.debug('hello %20s', world)"
         self.assert_patched(source, expected)
 
     def test_fstring_formatted_value_s_left(self):
-        source = 'log.debug(f"hello {world:<20}")'
-        expected = 'log.debug("hello %-20s", world)'
+        source = "log.debug(f'hello {world:<20}')"
+        expected = "log.debug('hello %-20s', world)"
         self.assert_patched(source, expected)
 
     def test_fstring_formatted_value_s_right(self):
-        source = 'log.debug(f"hello {world:>20}")'
-        expected = 'log.debug("hello %20s", world)'
+        source = "log.debug(f'hello {world:>20}')"
+        expected = "log.debug('hello %20s', world)"
         self.assert_patched(source, expected)
 
 
 class PatchIgnoreLazyTest(PatchTestBase):
     """Logging statements that should not be patched"""
+
     def test_ignore_lazy_1(self):
-        source = 'log.debug("%s", a)'
+        source = "log.debug('%s', a)"
         self.assert_unchanged(source)
 
     def test_ignore_lazy_3(self):
-        source = 'log.debug("%s %s %s", a, b, c)'
+        source = "log.debug('%s %s %s', a, b, c)"
         self.assert_unchanged(source)
 
 
 class PatchStrFormatTest(PatchTestBase):
     """Test patching statements that use ``str.format`` for formatting."""
+
     def test_preserve_format_string_strformat(self):
-        source = 'log.info("%s %d %f".format(a, b, c))'
-        expected = 'log.info("%s %d %f", a, b, c)'
+        source = "log.info('%s %d %f'.format(a, b, c))"
+        expected = "log.info('%s %d %f', a, b, c)"
         self.assert_patched(source, expected)
 
     def test_str_format_curly_braces_3(self):
-        source = 'log.debug("{} {} {}".format(a, b, c))'
-        expected = 'log.debug("%s %s %s", a, b, c)'
+        source = "log.debug('{} {} {}'.format(a, b, c))"
+        expected = "log.debug('%s %s %s', a, b, c)"
         self.assert_patched(source, expected)
 
     def test_ignore_str_format_with_variable(self):
@@ -148,28 +150,28 @@ class PatchStrFormatTest(PatchTestBase):
         self.assert_unchanged(source)
 
     def test_ignore_str_format_with_keywords(self):
-        source = 'log.debug("hello {world}".format(world="world"))'
+        source = "log.debug('hello {world}'.format(world='world'))"
         self.assert_unchanged(source)
 
     def test_ignore_str_format_formatted_value(self):
-        source = 'log.debug("{:.2f} {:^20}".format(3.1456, world))'
+        source = "log.debug('{:.2f} {:^20}'.format(3.1456, world))"
         self.assert_unchanged(source)
 
     def test_ignore_str_format_formatted_float_value(self):
-        source = 'log.debug("{:.2f}".format(3.1456))'
+        source = "log.debug('{:.2f}'.format(3.1456))"
         self.assert_unchanged(source)
 
     def test_ignore_str_format_formatted_s_value(self):
-        source = 'log.debug("{:20}".format(world))'
+        source = "log.debug('{:20}'.format(world))"
         self.assert_unchanged(source)
 
     def test_ignore_str_format_formatted_positionals(self):
-        source = 'log.debug("hello {0}".format(world))'
+        source = "log.debug('hello {0}'.format(world))"
         self.assert_unchanged(source)
 
     # def test_str_format_3_functions(self):
-    #     source = 'log.debug("%d %f %s".format(a+b, log(a), foo(bar(x,y))))'
-    #     expected = 'log.debug("%d %f %s", a + b, log(a), foo(bar(x, y)))'
+    #     source = "log.debug('%d %f %s".format(a+b, log(a), foo(bar(x,y))))"
+    #     expected = "log.debug('%d %f %s", a + b, log(a), foo(bar(x, y)))"
     #     self.assert_patched(source, expected)
 
 
